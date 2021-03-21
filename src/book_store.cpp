1451ade217d0cc762ca43a2a28c20e0299b6f5bf
Vivrina <i_videeva@mail.ru>
2021-03-21 20:04:42 +0300

    Done

--- src/book_store.cpp
+++ src/book_store.cpp
@@ -8,16 +8,35 @@ ResizeStorageStatus resize_storage(Book *&storage, int size, int new_capacity) {
   // здесь мог бы быть ваш разносторонний и многогранный код ...
   // Tip 1: проведите валидацию аргументов функции
   // Tip 2: не забудьте высвободить ранее выделенную память под хранилище
+  if (storage == nullptr){
+      return ResizeStorageStatus::NULL_STORAGE;
+  }
+  if (size<0){
+      return ResizeStorageStatus::NEGATIVE_SIZE;
+  }
+  if (new_capacity<=size){
+      return ResizeStorageStatus::INSUFFICIENT_CAPACITY;
+  }
+  Book *new_storage = new Book[new_capacity]();
+  for (int i=0; i<size; i++){
+      new_storage[i]=storage[i];
+  }
+  delete[] storage;
+  storage = new_storage;
   return ResizeStorageStatus::SUCCESS;
 }
 
 // 2. реализуйте конструктор ...
 BookStore::BookStore(const std::string &name) : name_{name} {
   // валидация аргумента
+
   if (name.empty()) {
     throw std::invalid_argument("BookStore::name must not be empty");
   }
 
+  name_ = name;
+  storage_capacity_ = kInitStorageCapacity;
+  storage_ = new Book[storage_capacity_];
   // здесь мог бы быть ваш сотрясающий землю и выделяющий память код ...
 }
 
@@ -25,6 +44,9 @@ BookStore::BookStore(const std::string &name) : name_{name} {
 BookStore::~BookStore() {
   // здесь мог бы быть ваш высвобождающий разум от негатива код ...
   // Tip 1: я свободен ..., словно память в куче: не забудьте обнулить указатель
+  delete[] storage_;
+  storage_capacity_ = 0;
+  storage_ = nullptr;
 }
 
 // 4. реализуйте метод ...
@@ -32,9 +54,14 @@ void BookStore::AddBook(const Book &book) {
   if (storage_size_ == storage_capacity_) {
     // здесь мог бы быть ваш умопомрачительный код ...
     // Tip 1: используйте функцию resize_storage_internal, задав новый размер хранилища
+      if (resize_storage_internal(storage_capacity_ + kCapacityCoefficient) != ResizeStorageStatus::SUCCESS){
+          return;
+      }
     // Tip 2: не забудьте обработать статус вызова функции
   }
   // Tip 3: не забудьте добавить книгу в наше бездонное хранилище ...
+  storage_[storage_size_] = book;
+  storage_size_++;
 }
 
 // РЕАЛИЗОВАНО
